e9134cf1fe38c4c4b0d8510f4b71c5d3706eee7e
Mariana Clare <momc@ac6-100.bullx>
2023-10-11 13:08:30 +0000

    allowing patched energy score to be called as a loss fn from the model

---
+++ jobscript-atos-debug.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#SBATCH --qos=ng
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=2
+#SBATCH --gpus-per-node=2
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=128G
+#SBATCH --time=48:00:00
+#SBATCH --output=ens-kcrps-mp-h4-test.out.%j
+
+# debugging flags (optional)
+# export NCCL_DEBUG=INFO
+# export PYTHONFAULTHANDLER=1
+export HYDRA_FULL_ERROR=1
+export NCCL_DEBUG=TRACE
+export TORCH_CPP_LOG_LEVEL=INFO
+export TORCH_DISTRIBUTED_DEBUG=DETAIL
+# export CUDA_LAUNCH_BLOCKING=1
+
+# on your cluster you might need these:
+# set the network interface
+# export NCCL_SOCKET_IFNAME=ib0,lo
+
+# Name and notes optional
+export WANDB_NAME="ens-energy-mpar-mc"
+export WANDB_NOTES="Energy score optimized ensemble forecasting"
+
+
+# generic settings
+CONDA_ENV=aifs_dev
+GITDIR=/perm/momc/AIFS/aifs-mono
+WORKDIR=$GITDIR
+
+cd $WORKDIR
+module load conda
+conda activate $CONDA_ENV
+srun aifs-ens-train hardware=atos_slurm --config-name=debug-ens-dmp-h4.yaml
