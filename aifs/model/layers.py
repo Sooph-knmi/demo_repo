39e8b3e5fef26b015c87bf73b78e3e7b294e519e
Simon Lang <simon.lang@ecmwf.int>
2023-11-27 16:12:02 +0000

    fix for edges in gnn proc for changing channels

--- aifs/model/layers.py
+++ aifs/model/layers.py
@@ -289,7 +289,7 @@ class GNNProcessorChunk(nn.Module):
         if self.in_channels != self.hidden_dim:
             x = self.proj_in(x)
             shape_new = change_channels_in_shape(shapes[0], self.hidden_dim)
-            shapes = (shape_new, shape_new)
+            shapes = (shape_new, shape_new, shapes[-1])
 
         for i in range(self.hidden_layers):
             x, edge_attr = self.proc[i](x, edge_index, edge_attr, shapes, batch_size, model_comm_group, size=size)
@@ -297,7 +297,7 @@ class GNNProcessorChunk(nn.Module):
         if self.out_channels != self.hidden_dim:
             x = self.proj_out(x)
             shape_new = change_channels_in_shape(shapes[0], self.out_channels)
-            shapes = (shape_new, shape_new)
+            shapes = (shape_new, shape_new, shapes[-1])
 
         return x, edge_attr
 
