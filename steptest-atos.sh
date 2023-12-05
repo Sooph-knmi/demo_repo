3043278a970c3737e5ef995f82e5d6456102469b
Mariana Clare <momc@ac6-100.bullx>
2023-12-05 09:03:45 +0000

    add debug file and configurable logger

--- steptest-atos.sh
+++ steptest-atos.sh
@@ -1,27 +1,29 @@
-#!/bin/bash
+#!/bin/bash -l
 
 #SBATCH --qos=ng
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=4
-#SBATCH --gpus-per-node=4
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=3
+#SBATCH --gpus-per-node=3
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=64G
 #SBATCH --time=00:15:00
-#SBATCH --output=debug.out.%j
+#SBATCH --output=steptest_debug.out.%j
 
 # debugging flags (optional)
 # export NCCL_DEBUG=INFO
 # export PYTHONFAULTHANDLER=1
 export HYDRA_FULL_ERROR=1
-#export NCCL_DEBUG=TRACE
-#export TORCH_CPP_LOG_LEVEL=INFO
-#export TORCH_DISTRIBUTED_DEBUG=DETAIL
+# export NCCL_DEBUG=TRACE
+# export TORCH_CPP_LOG_LEVEL=INFO
+# export TORCH_DISTRIBUTED_DEBUG=DETAIL
 # export CUDA_LAUNCH_BLOCKING=1
+
+export CUBLAS_WORKSPACE_CONFIG=:4096:8
+# export CUBLAS_WORKSPACE_CONFIG=:16:8
 
 # on your cluster you might need these:
 # set the network interface
 # export NCCL_SOCKET_IFNAME=ib0,lo
-
 
 # generic settings
 CONDA_ENV=aifs_dev
@@ -31,7 +33,4 @@ WORKDIR=$GITDIR
 cd $WORKDIR
 module load conda
 conda activate $CONDA_ENV
-
-
-wandb offline
-srun aifs-ens-train --config-name=debug
+aifs-ens-steptest
